9ce2df0c7afe68a8cd1b01956e4cfa81a45bc462
danvop <danvop@gmail.com>
2017-06-10 19:20:24 +1000

    just another commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Project Flyer
                 </div>
 
                 <div class="links">
