bb47470804820bd5e157e275f6193992000034e4
danvop <danvop@gmail.com>
2017-06-11 19:42:40 +1000

    flyer create form added

--- resources/views/flyers/create.blade.php
+++ resources/views/flyers/create.blade.php
@@ -2,8 +2,11 @@
 
 @section('content')
     <h1>Selling Your Home?</h1>
-    
-    <form>
-    
+    <hr>
+    <form method="POST" action="/flyers" enctype="multipart/form-data">
+        @include('flyers.form')
     </form>
+
+    
 @stop
+
